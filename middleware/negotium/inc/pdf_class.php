a6f9ae91aeeec92b923e79e83afdb9d9cc11bc40
aadaileh <ahmed.adaileh@gmail.com>
2018-05-01 22:16:28 +0200

    Finish PDF Creation class

--- middleware/negotium/inc/pdf_class.php
+++ middleware/negotium/inc/pdf_class.php
@@ -104,7 +104,7 @@ class PDF extends FPDF
 
     //SECTION: Experience
         $this->SetFont('Arial','B',16);
-        $this->Text(10,20,"EXPERIENCES (" . $experience . ")");
+        $this->Text(10,20,"WORK EXPERIENCES (" . $experience . ")");
         $this->Line(10,25,200,25);
         
         $x=13;
@@ -130,15 +130,56 @@ class PDF extends FPDF
         $this->Text($x+10,125,"- present"); 
 
         $this->SetFont('Arial','B',14);
-        $this->Text($x,131,"IT Consultant - Web Applications"); 
+        $this->Text($x,131,"Web Developer - Vignette"); 
 
         $this->SetFont('Arial','',10);
-        $this->Text($x,137,"at Vaillant Group Business Services");  
+        $this->Text($x,137,"at ARGES Consulting Co.");  
 
         $this->SetFont('Arial','',9);
         $this->SetY(145);
         $this->SetX($x-1);
-        $this->MultiCell(0,5,"I obtained the German IT Green Card where I got the chance to continue working with Vignette StoryServer as a platform for the content management system used in this company, one of my tasks was giving the necessary technical education to the other colleagues, enabling them to develop high quality applications. Then I got the chance to be involved in a PHP-MySql-Linux platform based CMS project. Since the company based on SAP, we have managed to get too different systems talking to each other through XML, WSDL and SOAP technologies. I gained more experience with operating systems administration; mainly Linux (SuSe and FreeBSD), also Oracle and MySql databases in administration, development and maintaining terms. Project management and conceptual project source planning is also part of my job. In Vaillant we are serving more than 13 Vaillant daughter companies all over Europe. all systems are based on php-MySql-Apache combination. Our websites are published under http://www.vaillant.com. Currently I am working on a project to manage the whole test results which obtained from LabView based testers from whole Europe. For this I am developing a SOAP-LabView based application. Also I am working directly in MySql optimization to handle a Terabytes data annually.");         
+        $this->MultiCell(0,5,"I started getting specialized in programming Internet within this period. My main job was developing content management systems for some newspapers and portals. We used mainly Vignette - Storyserver (TCL/TK)with MSSQL and ORACLE DB. I also gained some experience of project management and documentations. Ref.: 
+*) http://www.yenibir.com, [Career website],
+*) http://www.hurriyetim.com.tr [Newspaper],
+*) http://www.turkcell.com.tr [GSM Company]"); 
+
+    //SECTION: Education
+        $this->SetFont('Arial','B',16);
+        $this->Text(10,190,"EDUCATIONS");
+        $this->Line(10,195,200,195);
+        
+        $x=13;
+        $this->SetFont('Arial','',10);
+        $this->Text($x,205,"2017");
+        $this->Text($x+10,205,"- 2018");  
+
+        $this->SetFont('Arial','B',14);
+        $this->Text($x,211,"Master's degree, Software Engineering"); 
+
+        $this->SetFont('Arial','',10);
+        $this->Text($x,217,"at KINGSTON University London");  
+
+        $this->SetFont('Arial','',9);
+        $this->SetY(220);
+        $this->SetX($x-1);
+        $this->MultiCell(0,5,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");        
+    
+        $this->Line(20,235,180,235);
+
+        $this->SetFont('Arial','',10);
+        $this->Text($x,245,"1995");
+        $this->Text($x+10,245,"- 1999");  
+
+        $this->SetFont('Arial','B',14);
+        $this->Text($x,251,"Bachelor's degree, Computer Engineering"); 
+
+        $this->SetFont('Arial','',10);
+        $this->Text($x,257,"at Istanbul University");  
+
+        $this->SetFont('Arial','',9);
+        $this->SetY(263);
+        $this->SetX($x-1);
+        $this->MultiCell(0,5,"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia."); 
     }
 }
 
